c23d55302fd0ec6bcca98ddb69d2b8f863ee21e6
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-21 19:35:09 +0300

    [feat] initialize Values component

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Header } from './header/Header';
 export { default as Features } from './features/Features';
 export { default as Testemonials } from './testemonials/Testemonials';
 export { default as Footer } from './footer/Footer';
+export { default as Values } from './values/Values';
---
+++ web/src/components/values/Values.jsx
@@ -0,0 +1,7 @@
+import './values.css';
+
+const Values = () => {
+  return <div className="main__values section__padding"></div>;
+};
+
+export default Values;
