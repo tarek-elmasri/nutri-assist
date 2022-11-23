14f3f18b2afa615a85617c8e8186e7f951a64226
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-23 11:51:57 +0300

    [feat] links profileSlice with profile module

--- web/src/redux/features/profileSlice.ts
+++ web/src/redux/features/profileSlice.ts
@@ -1,31 +1,41 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import Profile, { ActivityLevel, Gender } from '../../modules/profile';
 
-export interface Profile {
-  weight: number;
-  height: number;
-  gender: 'male' | 'female';
-  age: number;
-  activityLevel: 'low' | 'medium' | 'high';
+interface ProfileState {
+  profile?: Profile;
 }
 
-const initialState: Profile = {
-  weight: 0,
-  height: 0,
-  gender: 'male',
-  age: 0,
-  activityLevel: 'low'
+interface ProfilePayload {
+  height: number;
+  weight: number;
+  age: number;
+  gender: Gender;
+  activityLevel: ActivityLevel;
+}
+
+const initialState: ProfileState = {
+  profile: undefined
 };
 
 const profileSlice = createSlice({
   name: 'profile',
-  initialState: { initialState },
+  initialState: initialState,
   reducers: {
-    setProfile: (state, action: PayloadAction<Profile>) => {
-      state = { ...state, ...action.payload };
+    updateProfile: (state, action: PayloadAction<ProfilePayload>) => {
+      const { weight, height, age, gender, activityLevel } = action.payload;
+      if (state.profile) {
+        state.profile.weight = weight;
+        state.profile.height = height;
+        state.profile.age = age;
+        state.profile.gender = gender;
+        state.profile.activityLevel = activityLevel;
+      } else {
+        state.profile = new Profile(height, weight, gender, age, activityLevel);
+      }
     }
   }
 });
 
-export const { setProfile } = profileSlice.actions;
+export const { updateProfile } = profileSlice.actions;
 
 export default profileSlice.reducer;
