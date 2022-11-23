61318cd502d6d2c3ceef2155f000ad5fe1aab8cc
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-23 07:48:36 +0300

    [feat] add Footer component

--- web/src/components/footer/Footer.tsx
+++ web/src/components/footer/Footer.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import logo from '../../assets/logo.png';
 
+import './footer.css';
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <div className="main__footer section__padding">
+      <div className="main__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="main__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+
+      <div className="main__footer-links">
+        <div className="main__footer-links_logo">
+          <img src={logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="main__footer-links_div">
+          <h4>Links</h4>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="main__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="main__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="main__footer-copyright">
+        <p>© 2021 Nutri.Assist. All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
