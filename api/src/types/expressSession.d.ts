40d6a96deb1973c758fc1a36e36e525f3cd530bf
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-28 02:27:52 +0300

    [feat] types declarations for sessions and requests

---
+++ api/src/types/expressSession.d.ts
@@ -0,0 +1,8 @@
+// merging express-session SessionData interface
+declare global {
+  declare module 'express-session' {
+    export interface SessionData {
+      user_id: string | undefined;
+    }
+  }
+}
