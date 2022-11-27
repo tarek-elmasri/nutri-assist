aabd1d9b2a11d9dc10275ca2f9aeb64fe8293838
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-28 02:27:01 +0300

    [feat] creates jwt verify helper

--- api/src/utils/jwtHelper.ts
+++ api/src/utils/jwtHelper.ts
@@ -4,3 +4,14 @@ export const jwtSign = (data: JwtPayload, expiresIn: string | number = '2h') =>
   jwt.sign({ ...data, expiresIn }, process.env.JWT_SECRET!, {
     algorithm: 'HS256'
   });
+
+export const jwtVerify = (token: string): JwtPayload | null => {
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!, {
+      algorithms: ['HS256']
+    });
+    return payload as JwtPayload;
+  } catch (error) {
+    return null;
+  }
+};
