16ac7064591ff71334a688b743d06f5f3a74cf58
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-28 18:08:00 +0300

    [fix] model associations forien key

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -3,26 +3,22 @@ import Client from './client';
 import Profile from './profile';
 import Serve from './serve';
 
-User.hasMany(Client, {
-  sourceKey: 'id',
-  foreignKey: 'clientId',
-  as: 'clients'
-});
+User.hasMany(Client, { foreignKey: 'userId', sourceKey: 'id', as: 'clients' });
 
-Client.belongsTo(User, { targetKey: 'id' });
+Client.belongsTo(User, { targetKey: 'id', foreignKey: 'userId' });
 Client.hasMany(Profile, {
   sourceKey: 'id',
   foreignKey: 'clientId',
   as: 'profiles'
 });
 
-Profile.belongsTo(Client, { targetKey: 'id' });
+Profile.belongsTo(Client, { targetKey: 'id', foreignKey: 'clientId' });
 Profile.hasMany(Serve, {
   sourceKey: 'id',
   foreignKey: 'profileId',
   as: 'serves'
 });
 
-Serve.belongsTo(Profile, { targetKey: 'id' });
+Serve.belongsTo(Profile, { targetKey: 'id', foreignKey: 'profileId' });
 
 export { User, Client, Profile, Serve };
